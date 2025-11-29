c666f2be914047061fedd37a008d48b94cfb3ec0
conan <knan36095@gmail.com>
2025-11-29 17:11:18 +0800

    补充项目文字说明

--- dx-openapi-sdk/src/test/java/config/ApiConfig.java
+++ dx-openapi-sdk/src/test/java/config/ApiConfig.java
@@ -3,22 +3,22 @@ package config;
 import lombok.Data;
 
 /**
- * 配置信息（需商户配置）
+ * 商户的api请求配置信息，需商户配置（配置信息可向我方项目运营申请）
  */
 @Data
 public class ApiConfig {
-
 
     /**
      * 商户请求网关
      */
     private  final String baseUrl = "https://api.wallet-dev.com";
     /**
-     * 商户api请求secret  用于sign加密
-     */
-    private  final String secret = "D2PQPllGEBOV4mcMxoKTM7foVpzqrjIx";
-    /**
      * 商户api请求 key 用于识别商户身份
      */
     private  final String key = "9yUreYgTRtit39Dy";
+    /**
+     * 商户api请求secret  用于sign加密
+     */
+    private  final String secret = "D2PQPllGEBOV4mcMxoKTM7foVpzqrjIx";
+
 }
